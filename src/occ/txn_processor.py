c6945f7756b47351116d4fb63c07844e96e5aae1
Jesson Yo <Aphostrophy@gmail.com>
2021-11-28 08:38:43 +0700

    added handlers and tc

---
+++ src/occ/txn_processor.py
@@ -0,0 +1,55 @@
+from typing import Any, Dict
+from occ.typings.transaction import Transaction
+
+from database import Database, DatabaseCacheExecutorWrapper
+
+class SerialTransactionExecutor:
+  def __init__(self, db: 'SerialDatabase' , txn: Transaction) -> None:
+    self.db = db
+    self.cached_db = DatabaseCacheExecutorWrapper(db)
+    self.txn = txn
+    self.start_ts = self.db._get_tsc()
+
+  def read_phase(self) -> None:
+    self.txn(self.cached_db)
+
+  def validate_and_write_phase(self) -> bool:
+    finish_ts = self.db._get_tsc()
+    '''
+      Check for every Tj s.t. startTS(Tj) < finishTS(Ti) < endTS(Tj) , write set of j doesn't intersect with read set of self (i)
+    '''
+    read_set_i = self.cached_db.get_read_set()
+    for ts in range(self.start_ts + 1, finish_ts + 1):
+      cached_db = self.db._get_transaction(ts)
+      write_set_j = cached_db.get_write_set()
+      if not write_set_j.isdisjoint(read_set_i):
+        return False
+
+    self.db._commit_transaction(self.cached_db)
+    return True
+
+
+class SerialDatabase(Database):
+  '''
+    Database with timestamp tracker
+  '''
+  def __init__(self):
+    Database.__init__(self)
+    self.transactions: Dict[int, DatabaseCacheExecutorWrapper] = {}
+    self.tsc: int = 0
+
+  def _get_tsc(self):
+    return self.tsc
+
+  def _get_transaction(self, tn:int) -> DatabaseCacheExecutorWrapper:
+    assert tn in self.transactions
+    return self.transactions[tn]
+
+  def _commit_transaction(self,db: DatabaseCacheExecutorWrapper) -> None:
+    self.tsc += 1
+    assert self.tsc not in self.transactions
+    self.transactions[self.tsc] = db
+    db.commit()
+
+  def begin(self, txn: Transaction) -> SerialTransactionExecutor:
+    return SerialTransactionExecutor(self, txn)
---
+++ src/occ_main.py
@@ -0,0 +1,54 @@
+from typing import List
+from occ.database import DatabaseCacheExecutorWrapper, Database
+from occ.txn_processor import SerialDatabase
+from occ.typings.transaction import Transaction
+
+def main():
+  print('This is OCC main driver')
+
+if __name__ == '__main__':
+  def init(db: DatabaseCacheExecutorWrapper) -> None:
+    db.write('x', 0)
+    db.write('y', 0)
+    db.write('z', 0)
+
+  def incr_vars(vs: List[str]) -> Transaction:
+    def txn(db: DatabaseCacheExecutorWrapper) -> None:
+      for v in vs:
+        x = db.read(v)
+        db.write(v, x + 1)
+    return txn
+
+  incr_x = incr_vars(['x'])
+  incr_y = incr_vars(['y'])
+  incr_z = incr_vars(['z'])
+  incr_all = incr_vars(['x', 'y', 'z'])
+
+  db = SerialDatabase()
+  assert(db.data == {})
+
+  t_init = db.begin(init)
+  t_init.read_phase()
+  assert(t_init.validate_and_write_phase())
+  assert(db.data == {'x': 0, 'y': 0, 'z': 0})
+
+  # t_1 and t_2 run concurrently and have conflicting read and write sets, so
+  # whichever transaction attempts to commit first (i.e. t_1) succeeds. The
+  # other (i.e. t_2) fails and is forced to abort.
+  t_1 = db.begin(incr_all)
+  t_2 = db.begin(incr_all)
+  t_1.read_phase()
+  t_2.read_phase()
+  assert(t_1.validate_and_write_phase())
+  assert(not t_2.validate_and_write_phase())
+  assert(db.data == {'x': 1, 'y': 1, 'z': 1})
+
+  # t_3 and t_4 run concurrently, but have disjoint read and write sets, so
+  # they can both commit.
+  t_3 = db.begin(incr_x)
+  t_4 = db.begin(incr_y)
+  t_3.read_phase()
+  t_4.read_phase()
+  assert(t_3.validate_and_write_phase())
+  assert(t_4.validate_and_write_phase())
+  assert(db.data == {'x': 2, 'y': 2, 'z': 1})
